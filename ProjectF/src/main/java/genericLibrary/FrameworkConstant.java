ddc64eb900eacb269e83180fae935d58abede05a
V.Nagendra Varma <V.Nagendra Varma@LAPTOP-HOV0GU3C>
2025-07-31 13:26:04 +0530

    Updated Scope

--- ProjectF/src/main/java/genericLibrary/FrameworkConstant.java
+++ ProjectF/src/main/java/genericLibrary/FrameworkConstant.java
@@ -5,6 +5,6 @@ public interface FrameworkConstant {
 	
 public static final long standardwait=20l;
 public static final  String propertypath="./src/test/resources/commondata.properties";
-public static final String excelpath = "./src/test/resources/Project.xlsx";
+public static final String excelpath = "./src/test/resources/Project1.xlsx";
 
 }
